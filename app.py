63163a6025810bea3037ce6c618eec470acd1f58
DonghoonLee <glee1228@naver.com>
2018-11-22 16:58:26 +0000

    포항가속기연구소 식당 게시판 크롤링 수정

--- app.py
+++ app.py
@@ -218,7 +218,12 @@ def message():
         res = requests.get(url)
         result = BeautifulSoup(res.content, 'html.parser')
         bab_tag = result.select('table > tbody > tr')
-        brd_num = str(bab_tag[0]['brd_num'])
+        brd_num = ""
+        brd_check = result.select('table > tbody > tr > td.titleLeft')
+        for i in range(0,2):
+            if "식당메뉴" in brd_check[i].text:
+                brd_num = str(bab_tag[i]['brd_num'])
+                break
         url2 = 'http://pal.postech.ac.kr/Board.pal?top=6&sub=12&sub2=0&method=boardView&pageMode=pal&mode=&brd_id=pal_cafeteria&brd_num={0}&currentPage=1&user_browser=msie_false&search_type=brd_subject&search_text=#'.format(brd_num)
         res2 = requests.get(url2)
         result2 = BeautifulSoup(res2.content,'html.parser')
